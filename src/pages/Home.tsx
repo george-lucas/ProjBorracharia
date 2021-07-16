07b6e79ea4964b1910bb8ccd139ea28fd8d58d87
devGEORGE <georgelucas347@hotmail.com>
2021-07-16 00:37:17 -0300

    alteracao posicao

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,55 @@
-import backgroundimage from '../assets/images/back.svg'
+import { useState } from 'react'
+import backimage from '../assets/images/back.svg'
 import '../styles/inicial.scss'
 
-export function Home() {
-    return(
-        <div id="auth">
-            <aside>
-             
-                <strong>NOME DA EMPRESA</strong>
-            </aside>
-            <main>
-                <div>
-                    <strong>LOGIN</strong>
 
-                    <form id="autenticacao">
-                        <div className="dados">
-                        <input type="text" 
-                            placeholder="Digite seu e-mail"/>
-                        </div>
-                        <div className="dados">
-                        <input type="text" 
-                            placeholder="Digite sua senha"/>
-                        </div>
-                        <div>
-                        <button type="submit" >
-                            Entrar
-                        </button>
-                        </div>
-                    </form>
-                </div>
-            </main>
-        </div>
+export function Home() {
+
+    const [email, setEmail] = useState('');
+    //const [password, setPassword] = useState('');
+
+    return(
+        //<img src = {backimage} alt = 'Plano de Fundo'/>
+            <div id="auth"> 
+       
+          
+                    <aside>
+                    
+                        <strong>NOME DA EMPRESA</strong>
+                    </aside>
+               
+                    <main>
+                            <strong>LOGIN</strong>
+
+                            <form id= "autenticacao">
+                                <div>
+                                <input type = "text"  className = 'dados'
+                                    placeholder= "Digite seu e-mail" 
+                                    value = {email}
+                                    onChange = {
+                                        (event) => {
+                                            setEmail(event.target.value)
+                                            }
+                                        }
+                                    />
+                                </div>
+                                <div>
+                                <input type = "password" className = 'dados'
+                                    placeholder = "Digite sua senha"
+                                //  value = {password}
+                                    />
+                                </div>
+                                <div className = 'buttonSubmit'>
+                                <button type="submit" className = 'buttonAuth'>
+                                    Entrar
+                                </button>
+                                </div>
+                            </form>
+                    </main>
+                  
+                    </div>
+
+
+  
     )
 }
