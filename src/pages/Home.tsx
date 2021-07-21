2dcd7debabd7a5a37ab7b05a3cdc3f6b7ae5d0a9
devGEORGE <georgelucas347@hotmail.com>
2021-07-21 01:22:46 -0300

    plano de fundo falta alteracao

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,55 +1,55 @@
 import { useState } from 'react'
-import backimage from '../assets/images/back.svg'
+import backimage from '../assets/images/back.jpg'
 import '../styles/inicial.scss'
-
 
 export function Home() {
 
     const [email, setEmail] = useState('');
-    //const [password, setPassword] = useState('');
+    const [password, setPassword] = useState('');
 
     return(
-        //<img src = {backimage} alt = 'Plano de Fundo'/>
+        <div className='background'>
+
+            <img src = {backimage}/>
+        
             <div id="auth"> 
-       
-          
-                    <aside>
-                    
-                        <strong>NOME DA EMPRESA</strong>
-                    </aside>
-               
-                    <main>
-                            <strong>LOGIN</strong>
 
-                            <form id= "autenticacao">
-                                <div>
-                                <input type = "text"  className = 'dados'
-                                    placeholder= "Digite seu e-mail" 
-                                    value = {email}
-                                    onChange = {
-                                        (event) => {
-                                            setEmail(event.target.value)
-                                            }
-                                        }
-                                    />
-                                </div>
-                                <div>
-                                <input type = "password" className = 'dados'
-                                    placeholder = "Digite sua senha"
-                                //  value = {password}
-                                    />
-                                </div>
-                                <div className = 'buttonSubmit'>
-                                <button type="submit" className = 'buttonAuth'>
-                                    Entrar
-                                </button>
-                                </div>
-                            </form>
-                    </main>
-                  
-                    </div>
-
-
-  
+                <aside>
+                    <strong>NOME DA EMPRESA</strong>
+                </aside>
+                <main>
+                    <strong>LOGIN</strong>
+                    <form id= "autenticacao">
+                        <div>
+                        <input type = "text"  className = 'dados'
+                            placeholder= "Digite seu e-mail" 
+                            value = {email}
+                            onChange = {
+                                (event) => {
+                                    setEmail(event.target.value)
+                                }
+                            }
+                        />
+                        </div>
+                        <div>
+                        <input type = "password" className = 'dados'
+                            placeholder = "Digite sua senha"
+                            value = {password}
+                            onChange = {
+                                (event) => {
+                                    setPassword(event.target.value)
+                                }
+                            }
+                        />
+                        </div>
+                        <div className = 'buttonSubmit'>
+                        <button type="submit" className = 'buttonAuth'>
+                            Entrar
+                        </button>
+                        </div>
+                    </form>
+                </main>
+            </div>
+        </div> 
     )
 }
